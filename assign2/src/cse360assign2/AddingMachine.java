5eafd5f4b02a30c662ff06b029b1058ef9b27a5f
treyf <treyf@DESKTOP-4NHD93P.3521.dhcp.asu.edu>
2019-10-07 15:42:52 -0700

    first

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,35 @@
+/*Trey Ferentheil
+Assignment 2
+CSE360-70641
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () { //return the current total
+		return 0;
+	}
+	
+	public void add (int value) { //adds parameter to the total variable
+		
+	}
+	
+	public void subtract (int value) { //subtract the parameter from the total variable
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
