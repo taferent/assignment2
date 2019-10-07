89f7357a0c3de96432621e47b1ed9529ddabcc11
treyf <treyf@DESKTOP-4NHD93P.3521.dhcp.asu.edu>
2019-10-07 16:29:17 -0700

    Finished assignment 2

--- assign2/src/cse360assign2/AddingMachine.java
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -7,29 +7,36 @@ package cse360assign2;
 
 public class AddingMachine {
 
-	private int total;
+	private int total; //total value
+	private String transHistory = "0"; //transaction history
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () { //return the current total
-		return 0;
+		return this.total;
 	}
 	
 	public void add (int value) { //adds parameter to the total variable
-		
+		this.total = this.total + value; //adds value to total
+		//updates transaction history
+		transHistory = transHistory + " + " + Integer.toString(value);
 	}
 	
 	public void subtract (int value) { //subtract the parameter from the total variable
-		
+		this.total = this.total - value; //subtracts value to total
+		//updates transaction history
+		transHistory = transHistory + " - " + Integer.toString(value);
 	}
 		
-	public String toString () {
-		return "";
+	public String toString () { //returns transaction history
+		return transHistory;
 	}
 
-	public void clear() {
-	
+	public void clear() { //clears history
+		total = 0;
+		transHistory = "0";
 	}
+	
 }
